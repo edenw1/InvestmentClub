8b828309c617de5b81afd54d8dc48e15ecf68bc9
Eden <edenw1@muskingum.edu>
2025-01-15 18:30:02 -0500

    Initial commit

---
+++ db.php
@@ -0,0 +1,18 @@
+<?php
+function dbConnect()
+//now in every document where you are using the database use this to establish database connetion 
+//require 'dbconnect.php';
+//dbConnect();
+
+{
+    global $pdo;
+    try {
+        $pdo = new PDO('mysql:host=localhost;dbname= investment_club', 'root', 'password');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->exec('SET NAMES "utf8"');
+    } catch (PDOException $e) {
+        echo "Database connection failed: " . $e->getMessage();
+        exit();
+    }
+}
+?>
