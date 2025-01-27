91f5ff1746244d9ac5ddec223e2ddcc2cd6b2c5c
Eden <edenw1@muskingum.edu>
2025-01-27 09:48:51 -0500

    login page (no doform yet)

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
 echo "Welcome, Investment Club!";
 ?>
+<form action="login.php" method="post">
+	<input type="submit" value="Register">
+</form>
---
+++ login.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>Returning User </title>
+<link rel="stylesheet" type="text/css" href="formatStyles.css">
+</head>
+<body>
+<div>
+ <h1>User Login</h1>
+    <form action="doform2.php" method="POST">
+        Username: <input type="text" name="username"><br>
+        Password: <input type="password" name="password"><br>
+        <input type="submit" value="Submit">
+    </form>
+	
+	
+	<br>
+Are you a new user? 
+<br>
+<form action="register.php" method="post">
+	<input type="submit" value="Register">
+</form>
+<form action="index.php" method="post">
+	<input type="submit" value="Continue as Guest">
+	</form>
+</div> 
+</body>
+</html>
