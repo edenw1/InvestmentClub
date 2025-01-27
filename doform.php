eee26fd8404eac13ad0b5415b4070e5ba02839f6
Eden <edenw1@muskingum.edu>
2025-01-27 10:11:18 -0500

    login page

---
+++ doform.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+REQUIRE 'db.php';
+dbConnect();
+$username = $_POST["username"];
+$password = $_POST["password"];
+$checkUsername = "SELECT * FROM Users WHERE username = :username";
+$stmt = $pdo->prepare($checkUsername);
+$stmt->bindParam(':username', $username);
+$stmt->execute();
+$user = $stmt->fetch(); 
+if (!$user) {
+     echo 'User is not found';
+} 
+else {
+	$storedPassword = $user['password'];
+	if (password_verify($password, $storedPassword)) {
+		session_regenerate_id(true);
+		$_SESSION['user_id'] = $user['user_id'];
+		$_SESSION['admin'] = $user['admin'];	
+			echo 'Login Successful';
+			header("Location: index.php");
+			exit();
+		
+	} else {
+      echo 'Incorrect username or password';
+	  sleep(3);
+	  header("Location: login.php");
+	  exit();
+}
+}
+
+?>
---
+++ login.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>Returning User </title>
+<link rel="stylesheet" type="text/css" href="formatStyles.css">
+</head>
+<body>
+<div>
+ <h1>User Login</h1>
+    <form action="doform.php" method="POST">
+        Username: <input type="text" name="username"><br>
+        Password: <input type="password" name="password"><br>
+        <input type="submit" value="Submit">
+    </form>
+	
+<br>
+<form action="index.php" method="post">
+	<input type="submit" value="Continue as Guest">
+	</form>
+</div> 
+</body>
+</html>
