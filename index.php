f76c9c1c0818185bb2e633adfcbef922047990c6
Eden <edenw1@muskingum.edu>
2025-01-27 10:14:34 -0500

    test

--- index.php
+++ index.php
@@ -2,9 +2,5 @@
 echo "Welcome, Investment Club!";
 ?>
 <form action="login.php" method="post">
-<<<<<<< HEAD
 	<input type="submit" value="Login">
-=======
-	<input type="submit" value="Register">
->>>>>>> 91f5ff1746244d9ac5ddec223e2ddcc2cd6b2c5c
 </form>
