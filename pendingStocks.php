fc90c70aaf715a1d3d565791d260cd5623e90e47
Eden <edenw1@muskingum.edu>
2025-02-07 08:41:24 -0500

    index.html

--- pendingStocks.php
+++ pendingStocks.php
@@ -7,7 +7,7 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 if (!isset($_SESSION['admin'])) {
-    header("Location: indexx.html");
+    header("Location: index.html");
     exit();
 }
 
@@ -59,6 +59,6 @@ $stmt->execute();
 $proposals = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
-<form action="indexx.html" method="post">
+<form action="index.html" method="post">
     <input type="submit" value="Back to Home">
 </form>
--- presentations.php
+++ presentations.php
@@ -46,7 +46,7 @@ dbConnect();
         <input type="submit" value="Upload Presentation">
     </form>
 
-    <form action="indexx.html" method="post">
+    <form action="index.html" method="post">
         <input type="submit" value="Back to Home">
     </form>
     <br>
