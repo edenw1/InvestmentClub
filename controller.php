d2e270023f86f656fae89efb94df7a9d1e336dc6
Devin Roebuck <droebuck1@muskingum.edu>
2025-03-31 13:48:51 -0400

    changed title

--- controller.php
+++ controller.php
@@ -76,7 +76,7 @@ function handlePortfolio($twig, $user, $isAuthenticated) {
             'trends' => getTrends($row['symbol'])
         ], $activeStocks);
         
-        echo $twig->render('index.html.twig', ['stocks' => $stocks, 'user' => $user]);
+        echo $twig->render('index.html.twig', ['stocks' => $stocks, 'user' => $user], 'page_title' => $- Portfolio Page);
     } catch (Exception $e) {
         echo "Error portfolio: " . $e->getMessage();
     }
