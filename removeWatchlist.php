77e50ba8c1ab7bef6be51f9b32fb5276f8f00387
CivilizedCoder <twilson2@muskingum.edu>
2025-04-07 10:18:45 -0400

    remove from watchlist WIP, css fixes

---
+++ removeWatchlist.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+require 'databaseFunctions.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $symbol = $_POST['symbol'];
+    $name = $_POST['name'];
+
+    if (!empty($symbol) && !empty($name)) {
+        if (removeWatchlist($symbol, $name)) {
+            header("Location: /");
+            exit();
+        } else {
+            echo "Error aremoving stock from watchlist";
+            ?>
+            <a href="/">Home</a>
+            <?php
+        }
+    } else {
+        echo "Error: Symbol and name are required.";
+        ?>
+        <a href="/">Home</a>
+        <?php
+    }
+}
+?>
